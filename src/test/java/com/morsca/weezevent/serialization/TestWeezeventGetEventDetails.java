92995c6eab84347619942f41bfce28a7418d7382
morsca <fabien.scagnelli@gmail.com>
2015-05-31 21:22:42 +0200

    Debug why travic-ci.org failed

--- src/test/java/com/morsca/weezevent/serialization/TestWeezeventGetEventDetails.java
+++ src/test/java/com/morsca/weezevent/serialization/TestWeezeventGetEventDetails.java
@@ -3,7 +3,7 @@ package com.morsca.weezevent.serialization;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Locale;
+import java.util.TimeZone;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -17,12 +17,16 @@ public class TestWeezeventGetEventDetails {
 
 	private final ObjectMapper mapper = new ObjectMapper();
 	
-	private final DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss", Locale.ROOT);
+	private final DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	
+	{
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));	
+	}
 	
 	@Before
 	public void before() {
 		mapper.setPropertyNamingStrategy(PropertyNamingStrategy.CAMEL_CASE_TO_LOWER_CASE_WITH_UNDERSCORES);
-		mapper.setDateFormat(new SimpleDateFormat("yyy-MM-dd HH:mm:ss", Locale.ROOT));
+		mapper.setDateFormat(new SimpleDateFormat("yyy-MM-dd HH:mm:ss"));
 	}
 	
 	@Test
@@ -30,7 +34,7 @@ public class TestWeezeventGetEventDetails {
 		WeezeventEventDetailsResponse eventDetailsResponse = mapper.readValue(ClassLoader.getSystemResourceAsStream("getEventDetails.json"), WeezeventEventDetailsResponse.class);
 		
 		Assert.assertNotNull(eventDetailsResponse);
-		Date date1 = dateFormat.parse("27/05/2013 18:02:16");
+		Date date1 = dateFormat.parse("27/05/2013 18:02");
 		Date date2 = eventDetailsResponse.getLastUpdate();
 		System.out.println("date1 : " + date1 + "; time : " + date1.getTime());
 		System.out.println("date2 : " + date2 + "; time : " + date2.getTime());
