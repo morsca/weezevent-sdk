2e915b0dcd65022163fa6ee666e84abfb9300657
morsca <fabien.scagnelli@gmail.com>
2015-05-31 21:29:44 +0200

    Debug why travis-ci.org failed

--- src/test/java/com/morsca/weezevent/serialization/TestWeezeventGetEventDetails.java
+++ src/test/java/com/morsca/weezevent/serialization/TestWeezeventGetEventDetails.java
@@ -19,14 +19,11 @@ public class TestWeezeventGetEventDetails {
 	
 	private final DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 	
-	{
-		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));	
-	}
-	
 	@Before
 	public void before() {
 		mapper.setPropertyNamingStrategy(PropertyNamingStrategy.CAMEL_CASE_TO_LOWER_CASE_WITH_UNDERSCORES);
 		mapper.setDateFormat(new SimpleDateFormat("yyy-MM-dd HH:mm:ss"));
+		mapper.setTimeZone(TimeZone.getDefault());
 	}
 	
 	@Test
@@ -34,7 +31,7 @@ public class TestWeezeventGetEventDetails {
 		WeezeventEventDetailsResponse eventDetailsResponse = mapper.readValue(ClassLoader.getSystemResourceAsStream("getEventDetails.json"), WeezeventEventDetailsResponse.class);
 		
 		Assert.assertNotNull(eventDetailsResponse);
-		Date date1 = dateFormat.parse("27/05/2013 18:02");
+		Date date1 = dateFormat.parse("27/05/2013 18:02:16");
 		Date date2 = eventDetailsResponse.getLastUpdate();
 		System.out.println("date1 : " + date1 + "; time : " + date1.getTime());
 		System.out.println("date2 : " + date2 + "; time : " + date2.getTime());
